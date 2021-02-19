ab3f5f5573fc3ede6d9311a494c441466571dc2b
Mike Davidson <captain@mikeddavison.com>
2021-02-19 04:44:40 -0500

    Improved SVG look

--- src/ui/HBBoard/HBCard.tsx
+++ src/ui/HBBoard/HBCard.tsx
@@ -10,34 +10,29 @@ export default function HBCard({ suit, rank }: HBCardProps) {
   let colorData = colors[suit];
   const num = rank;
   return (
-    <div style={{ margin: "2.5px" }}>
-      <svg height="150" width="110">
+      <svg height="150" width="110" style={{ margin: "2.5px" }}>
         <defs>
-          <filter id="shadow">
-            <feDropShadow dx="0" dy="0" stdDeviation="2" result="shadow" />
-            <feMerge>
-              <feMergeNode in="shadow" />
-              <feMergeNode in="SourceGraphic" />
-            </feMerge>
+          <filter id="outline">
+          <feComponentTransfer>
+            <feFuncR type="linear" slope="0"/>
+            <feFuncG type="linear" slope="0"/>
+            <feFuncB type="linear" slope="0"/>
+          </feComponentTransfer>
+          <feMorphology operator="dilate" radius="1"/>
+          <feMerge>
+            <feMergeNode/>
+            <feMergeNode in="SourceGraphic"/>
+          </feMerge>
           </filter>
         </defs>
         <rect x="5%" y="5%" width="90%" height="90%" fill={colorData.back} strokeWidth="2.5%" stroke={colorData.fill} rx="5%" />
-        <text fill={colorData.fill} className='rank' x='20%' y='20%' fontSize='50px' textAnchor='middle' dominantBaseline='central' strokeWidth="1.5" stroke="black">{num}</text>
-        {/*Center Pip*/ num % 2 === 1 ? <image href={colorData.pip} height="15%" x="38%" y="42.5%"/> : undefined}
-        {   /*Top Pip*/       num > 1 ? <image href={colorData.pip} height="15%" x="38%" y="20%"  /> : undefined}
-        {/*Bottom Pip*/       num > 1 ? <image href={colorData.pip} height="15%" x="38%" y="65%"  /> : undefined}
-        {  /*Left Pip*/       num > 3 ? <image href={colorData.pip} height="15%" x="10%" y="42.5%"/> : undefined}
-        { /*Right Pip*/       num > 3 ? <image href={colorData.pip} height="15%" x="65%" y="42.5%"/> : undefined}
-        <text rotate="180" fill={colorData.fill} className='rank' x='105%' y='55%' fontSize='50px' textAnchor='middle' dominantBaseline='central' strokeWidth="1.5" stroke="black">{num}</text>
+        <text fill={colorData.fill} className='rank' x='20%' y='20%' fontSize='50px' textAnchor='middle' dominantBaseline='central' filter="url(#outline)">{num}</text>
+        {/*Center Pip*/ num % 2 === 1 ? <image href={colorData.pip} height="17.5%" x="37.5%" y="40%" filter="url(#outline)"/> : undefined}
+        {   /*Top Pip*/       num > 1 ? <image href={colorData.pip} height="12.5%" x="40.5%" y="20%" filter="url(#outline)"/> : undefined}
+        {/*Bottom Pip*/       num > 1 ? <image href={colorData.pip} height="12.5%" x="40.5%" y="65%" filter="url(#outline)"/> : undefined}
+        {  /*Left Pip*/       num > 3 ? <image href={colorData.pip} height="12.5%" x="12.5%" y="42.5%" filter="url(#outline)"/> : undefined}
+        { /*Right Pip*/       num > 3 ? <image href={colorData.pip} height="12.5%" x="67.5%" y="42.5%" filter="url(#outline)"/> : undefined}
+        <text rotate="180" fill={colorData.fill} className='rank' x='105%' y='55%' fontSize='50px' textAnchor='middle' dominantBaseline='central' filter="url(#outline)">{num}</text>
       </svg>
-    </div>
   );
 }
-
-{/*<div className="cardNumber cardNumber-top">{num}</div>
-<img className="cardPip cardPip1" src={colorData.pip} alt="" hidden={!(num === 4 || num === 5)} />
-<img className="cardPip cardPip2" src={colorData.pip} alt="" hidden={num === 2 || num === 4} />
-<img className="cardPip cardPip3" src={colorData.pip} alt="" hidden={!(num === 4 || num === 5)} />
-<img className="cardPip cardPip4" src={colorData.pip} alt="" hidden={num === 1} />
-<img className="cardPip cardPip5" src={colorData.pip} alt="" hidden={num === 1} />
-*/}
--- src/ui/HBBoard/HBHandsArea.tsx
+++ src/ui/HBBoard/HBHandsArea.tsx
@@ -5,7 +5,7 @@ import { GameUIContext } from '../ReactFrontendInterface'
 export default function HBHandsArea() {
   const playerNames = React.useContext(GameUIContext).getPlayerNames();
   return (
-    <div className="HBHandsArea">
+    <div className="HBHandsArea" style={{gridTemplateRows:`repeat(${playerNames.length}, min-content)`}}>
       {playerNames.map((n, i) => <HBHand player={i} key={i} />)}
     </div>
   )
