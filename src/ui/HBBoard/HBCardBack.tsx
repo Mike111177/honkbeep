17a180b36fd14fb59213ff74b65202cf2bca98fb
Mike Davidson <captain@mikeddavison.com>
2021-02-26 16:20:33 -0500

    Draw unknown card

--- src/ui/HBBoard/HBCardBack.tsx
+++ src/ui/HBBoard/HBCardBack.tsx
@@ -5,5 +5,23 @@ type HBCardBackProps = {
 } & ComponentProps<"svg">;
 
 export default function HBCardBack({ suits, ...props }: HBCardBackProps) {
-  return <></>;  
+  return (
+    <svg {...props}>
+      <defs>
+        <filter id="outline">
+          <feComponentTransfer>
+            <feFuncR type="linear" slope="0" />
+            <feFuncG type="linear" slope="0" />
+            <feFuncB type="linear" slope="0" />
+          </feComponentTransfer>
+          <feMorphology operator="dilate" radius="1" />
+          <feMerge>
+            <feMergeNode />
+            <feMergeNode in="SourceGraphic" />
+          </feMerge>
+        </filter>
+      </defs>
+      <rect x="5%" y="5%" width="90%" height="90%" fill="#cccccc" strokeWidth="2.5%" stroke="#777777" rx="5%" />
+    </svg>
+  );
 }
--- src/ui/HBBoard/HBDeckCard.tsx
+++ src/ui/HBBoard/HBDeckCard.tsx
@@ -2,18 +2,15 @@ import { useContext, useEffect, useState } from "react";
 
 import HBCardFront from "./HBCardFront";
 import { GameUIContext } from "../ReactFrontendInterface";
+import HBCardBack from "./HBCardBack";
 
 
 export default function HBDeckCard({ index }: any) {
   const context = useContext(GameUIContext);
-  const getCurrentDisplayProps = () => context.getCardDisplayableProps(index);
-  const [cardInfo, setDisprops] = useState(getCurrentDisplayProps());
-  useEffect(() => {
-    const callback = () => setDisprops(getCurrentDisplayProps());
-    const removeFunc = () => { context.off("game-update", callback) };
-    context.on("game-update", callback);
-    return removeFunc;
-  });
-  return <HBCardFront height="150" width="110" style={{ margin: "2.5px", userSelect: "none" }} {...cardInfo} />;
-
+  if (context.isCardRevealed(index)) {
+    let cardInfo = context.getCardDisplayableProps(index);
+    return <HBCardFront height="150" width="110" style={{ margin: "2.5px", userSelect: "none" }} {...cardInfo} />;
+  } else {
+    return <HBCardBack height="150" width="110" style={{ margin: "2.5px", userSelect: "none" }} suits={[]} />;
+  }
 }
