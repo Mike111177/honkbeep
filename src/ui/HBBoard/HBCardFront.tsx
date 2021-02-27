b31bc406f54141b2f50d231d22691f98c108b855
Mike Davidson <captain@mikeddavison.com>
2021-02-26 21:38:17 -0500

    Mathematically perfect card drawing

--- src/ui/HBBoard/HBCardFront.tsx
+++ src/ui/HBBoard/HBCardFront.tsx
@@ -2,11 +2,35 @@ import "./HBCard.scss";
 import colors from "../colors";
 import pips from "./pips";
 import { ComponentProps } from "react";
+import { vecAdd, vecMul, vecSub } from "../util/Vector";
 
 export type HBCardProps = {
   rank: number;
   suit: string;
 } & ComponentProps<"svg">
+
+//Tweakables
+const view = { x: 110, y: 150 };
+const rPipHeight = 17.5;
+const cPipHeight = 27.5;
+const pipDist = { x: 30, y: 35 };
+const numOffset = { x: 22.5, y: 30 };
+const numSize = 50;
+
+//Calculated Constants
+const viewBox = `0 0 ${view.x} ${view.y}`;
+const mid = vecMul(view, 0.5);
+
+const pipCorner = vecSub(mid, pipDist);
+
+const rPipOff = { x: - rPipHeight / 2, y: - rPipHeight / 2 };
+const cPipOff = { x: - cPipHeight / 2, y: - cPipHeight / 2 };
+
+const pipFlip = `rotate(180 ${mid.x} ${mid.y})`;
+
+const pipT = vecAdd({ x: mid.x, y: pipCorner.y }, rPipOff);
+const pipR = vecAdd({ x: pipCorner.x, y: mid.y }, rPipOff);
+const pipC = vecAdd(mid, cPipOff);
 
 //For rendering frontface of absolutley known card
 //TODO: The bottom and right-hand pips should be flipped 180 degrees
@@ -16,7 +40,7 @@ export default function HBCardFront({ suit, rank, ...props }: HBCardProps) {
   let pip = pips[suit];
   const num = rank;
   return (
-    <svg {...props} viewBox="0 0 110 150">
+    <svg {...props} viewBox={viewBox}>
       <defs>
         <filter id="outline">
           <feComponentTransfer>
@@ -32,19 +56,20 @@ export default function HBCardFront({ suit, rank, ...props }: HBCardProps) {
         </filter>
       </defs>
       <rect x="5%" y="5%" width="90%" height="90%" fill={colorData.back} strokeWidth="2.5%" stroke={colorData.fill} rx="5%" />
-      <text fill={colorData.fill} className='rank' x='20%' y='20%' fontSize='50px' textAnchor='middle' dominantBaseline='central' filter="url(#outline)">{num}</text>
-      {/*Center Pip*/         num % 2 === 1 ? <>
-        <image href={pip} height="17.5%" x="37.5%" y="40%" filter="url(#outline)" />
+      <text fill={colorData.fill} {...numOffset} fontSize={numSize} textAnchor='middle' dominantBaseline='central' filter="url(#outline)">{num}</text>
+      <text fill={colorData.fill} {...numOffset} fontSize={numSize} textAnchor='middle' dominantBaseline='central' filter="url(#outline)" transform={pipFlip}>{num}</text>
+      { num % 2 === 1 ? /*Center Pip*/ <>
+        <image href={pip} height={cPipHeight} {...pipC} filter="url(#outline)" />
       </> : undefined}
-      {/*Top And Bottom Pip*/ num > 1 ? <>
-        <image href={pip} height="12.5%" x="40.5%" y="20%" filter="url(#outline)" />
-        <image href={pip} height="12.5%" x="40.5%" y="20%" filter="url(#outline)" transform="rotate(180 55 75)" />
+      { num > 1 ? /*Top And Bottom Pip*/ <>
+        <image href={pip} height={rPipHeight} {...pipT} filter="url(#outline)" />
+        <image href={pip} height={rPipHeight} {...pipT} filter="url(#outline)" transform={pipFlip} />
       </> : undefined}
-      {/*Left And Right Pip*/ num > 3 ? <>
-        <image href={pip} height="12.5%" x="12.5%" y="42.5%" filter="url(#outline)" />
-        <image href={pip} height="12.5%" x="12.5%" y="42.5%" filter="url(#outline)" transform="rotate(180 55 75)"/>
+      { num > 3 ? /*Left And Right Pip*/ <>
+        <image href={pip} height={rPipHeight} {...pipR} filter="url(#outline)" />
+        <image href={pip} height={rPipHeight} {...pipR} filter="url(#outline)" transform={pipFlip} />
       </> : undefined}
-      <text rotate="180" fill={colorData.fill} className='rank' x='105%' y='55%' fontSize='50px' textAnchor='middle' dominantBaseline='central' filter="url(#outline)">{num}</text>
+
     </svg>
   );
 }
