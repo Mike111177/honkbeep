40bba66303b686060b2d137718391c05a5478639
Mike Davidson <captain@mikeddavison.com>
2021-03-08 15:33:40 -0500

    Documented ArrayUtils

--- src/util/ArrayUtil.ts
+++ src/util/ArrayUtil.ts
@@ -1,7 +1,23 @@
-function iota(length: number): number[] {
-  return [...Array(length).keys()];
+/**
+ * Makes an array with ascending numbers at a requested length. Provide a starting value to offset the first value.
+ * @param {number} length Length of array
+ * @param {number} [start=0] Initial value
+ * @returns {number} Array with ascending numbers
+ */
+function iota(length: number, start:number = 0): number[] {
+  const arr = [...Array(length).keys()];
+  if (start > 0) return arr.map(i => i + start);
+  else return arr;
 }
 
+/**
+ * Creates an array at requested length, filled with a provided value.
+ * You can also provide a factory function for the value parameter.
+ * @template T
+ * @param {number} length Requested length
+ * @param {(T | ()=>T)} value Item to fill array with or a factory function
+ * @returns {T[]} Array filled with requested values
+ */
 function fill<T>(length: number, value: ()=>T): T[];
 function fill<T>(length: number, value: T): T[];
 function fill(length: number, value: any): any[] {
@@ -12,8 +28,6 @@ function fill(length: number, value: any): any[] {
     return iota(length).map(() => value);
   }
 }
-
-
 
 const ArrayUtil = {
   iota,
