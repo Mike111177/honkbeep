755841901a718188aecdc693ff64f4a179c3c20c
Mike111177 <captain@mikeddavidson.com>
2021-03-24 17:35:30 -0400

    Improved sorting of discard pile

--- src/ui/HBBoard/DiscardPile/DiscardPile.tsx
+++ src/ui/HBBoard/DiscardPile/DiscardPile.tsx
@@ -9,37 +9,38 @@ import styles from "./DiscardPile.module.css";
 import darkregion from "../DarkRegion.module.css";
 
 export default function DiscardPile() {
-  const [cards, shuffleOrder, deck] = useBoardState((boardState) => {
+  const [cards, shuffleOrder, deck, suits] = useBoardState((boardState) => {
     return [
       boardState.viewTurn.discardPile,
       boardState.shuffleOrder,
       boardState.deck,
+      boardState.definition.variant.suits,
     ];
   });
   const ref = useFloatArea(["discardPile"], { dropZone: true });
 
-  //TODO: Fix this..........
-  const cardOrder = useMemo(
+  const targets = useMemo(
     () =>
-      cards
-        .map((index) => ({
-          index,
-          ...deck.getCard(shuffleOrder[index]),
-        }))
-        .sort((a, b) =>
-          a.suit < b.suit ? -1 : a.suit > b.suit ? 1 : a.rank - b.rank
-        )
-        .map((i) => i.index),
-    [cards, deck, shuffleOrder]
+      cards.map((card, i) => {
+        const cardValue = deck.getCard(shuffleOrder[card]);
+        const suitNumber = suits.findIndex((s) => s === cardValue.suit) + 1;
+        return (
+          <CardTarget
+            key={i}
+            areaPath={["discard", card]}
+            style={{ gridColumn: suitNumber }}
+          />
+        );
+      }),
+    [cards, deck, shuffleOrder, suits]
   );
+
   return (
     <div
       className={classNames(styles.DiscardPile, darkregion.DarkRegion)}
       ref={ref}
     >
-      {cardOrder.map((i) => (
-        <CardTarget key={i} areaPath={["discard", i]} />
-      ))}
+      {targets}
     </div>
   );
 }
