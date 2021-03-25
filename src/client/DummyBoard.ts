1bcf11eda3d490d3df680ff8b4e44b478446a9d9
Mike111177 <captain@mikeddavidson.com>
2021-03-24 21:45:41 -0400

    Added controls to clueArea Story

--- src/client/DummyBoard.ts
+++ src/client/DummyBoard.ts
@@ -1,5 +1,10 @@
 import { getShuffledOrder } from "../game/DeckBuilding";
-import { GameAttempt, GameEvent, GameEventType } from "../game/GameTypes";
+import {
+  GameAttempt,
+  GameDefinition,
+  GameEvent,
+  GameEventType,
+} from "../game/GameTypes";
 import Board from "./Board";
 import {
   initBoardState,
@@ -12,15 +17,17 @@ import UserAction from "./types/UserAction";
 type UserActionCallback = (action: UserAction) => void;
 export default class DummyBoard extends Board {
   private callback: UserActionCallback;
-  constructor(callback: UserActionCallback) {
-    const definition = {
+  constructor(
+    callback: UserActionCallback,
+    definition: GameDefinition = {
       variant: {
         suits: ["Red", "Yellow", "Green", "Blue", "Purple"],
         numPlayers: 4,
         handSize: 4,
       },
       playerNames: ["Alice", "Bob", "Cathy", "Donald"],
-    };
+    }
+  ) {
     const state0 = initBoardState(definition);
     const dealEvent: GameEvent = { turn: 0, type: GameEventType.Deal };
     const { order } = getShuffledOrder(state0.deck.length);
--- src/ui/HBBoard/ClueArea/ClueArea.stories.tsx
+++ src/ui/HBBoard/ClueArea/ClueArea.stories.tsx
@@ -1,28 +1,51 @@
-import { Meta } from "@storybook/react";
-import { useState } from "react";
+import { Meta, Story } from "@storybook/react";
 import HBClueArea from "./ClueArea";
 import DummyBoard from "../../../client/DummyBoard";
 import { BoardContext } from "../../BoardContext";
+
+import colors from "../../colors.json";
 
 export default {
   title: "Board Components/Clue Area",
   component: HBClueArea,
   parameters: {
     layout: "centered",
-    controls: { disabled: true, hideNoControlsWarning: true },
   },
   argTypes: {
     onAttempt: { action: "User Action", table: { disable: true } },
+    suits: {
+      control: {
+        type: "check",
+        options: colors.map((c) => c.name),
+      },
+    },
+    players: {
+      type: "array",
+    },
   },
 } as Meta;
 
-export const ClueAreaStory = ({ onAttempt }: any) => {
-  const [board] = useState(() => new DummyBoard(onAttempt));
+export const ClueAreaStory: Story<any> = ({
+  onAttempt,
+  suits,
+  players,
+}: any) => {
+  const gameDef = {
+    variant: {
+      suits,
+      numPlayers: players.length,
+      handSize: 4,
+    },
+    playerNames: players,
+  };
   return (
-    <BoardContext.Provider value={board}>
+    <BoardContext.Provider value={new DummyBoard(onAttempt, gameDef)}>
       <HBClueArea />
     </BoardContext.Provider>
   );
 };
-
 ClueAreaStory.storyName = "Clue Area";
+ClueAreaStory.args = {
+  suits: ["Red", "Yellow", "Green", "Blue", "Purple"],
+  players: ["Alice", "Bob", "Cathy", "Donald"],
+};
