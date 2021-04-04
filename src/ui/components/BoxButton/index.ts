a07e6a3c6b3bac902d339cda802da583d31dcf2e
Mike111177 <captain@mikeddavidson.com>
2021-04-04 13:59:14 -0400

    export styles from BoxButton

--- src/ui/components/BoxButton/index.ts
+++ src/ui/components/BoxButton/index.ts
@@ -1,3 +1,4 @@
 import BoxButton from "./BoxButton";
 export default BoxButton;
+export {default as BoxButtonStyles} from "./BoxButton.module.css"
 export type { BoxButtonProps } from "./BoxButton";
