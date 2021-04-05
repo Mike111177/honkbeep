0c6b6329b77ad53ff9f58e16b378ad97aecce1df
Mike111177 <captain@mikeddavidson.com>
2021-04-05 19:25:54 -0400

    Fixed yarn start

---
+++ src/modules.d.ts
@@ -0,0 +1,19 @@
+declare module "*.css" {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module "*.jpg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpeg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.svg" {
+  const src: string;
+  export default src;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -5,74 +5,105 @@ const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  devtool: false,
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "static/js/[name].[contenthash:8].js",
-    chunkFilename: "static/js/[name].[contenthash:8].chunk.js",
-    clean: true,
-  },
-  entry: "./src/index.tsx",
-  resolve: { extensions: [".ts", ".tsx", ".js", ".css"] },
-  module: {
-    rules: [
+const buildDir = path.resolve(__dirname, "build");
+
+module.exports = (env) => {
+  //Get mode
+  const mode = env.production ? "production" : "development";
+  const isProduction = mode === "production";
+  const isDevelopment = !isProduction;
+
+  //Init rules
+  const CSSRule = {
+    test: /\.css$/,
+    use: [
+      isProduction ? MiniCssExtractPlugin.loader : "style-loader",
       {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.jpe?g|\.svg/,
-        loader: "file-loader",
+        loader: "css-loader",
         options: {
-          name: "static/media/[name].[hash:8].[ext]",
+          modules: {
+            localIdentName: isProduction
+              ? "[hash:base64:10]"
+              : "[local]-[hash:base64:5]",
+          },
         },
       },
-      { test: /\.tsx?$/, use: "ts-loader" },
     ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "static/css/[name].[contenthash:8].css",
-      chunkFilename: "static/css/[name].[contenthash:8].chunk.css",
-    }),
-    new HtmlWebpackPlugin({ template: "./public/index.html" }),
-    new webpack.SourceMapDevToolPlugin({
-      filename: "[name].js.map",
-      //exclude: ["vendor.js"],
-    }),
-  ],
-  optimization: {
-    minimizer: [
-      new CssMinimizerPlugin(),
-      new TerserPlugin({
-        terserOptions: {
-          parse: {
-            ecma: 8,
-          },
-          compress: {
-            ecma: 5,
-            warnings: false,
-            comparisons: false,
-            inline: 2,
-          },
-          output: {
-            ecma: 8,
-            comments: false,
-            ascii_only: true,
+  };
+
+  //Init Plugins
+  const plugins = [new HtmlWebpackPlugin({ template: "./src/index.html" })];
+
+  //Things to add for production build
+  if (isProduction) {
+    //Minify css
+    plugins.unshift(
+      new MiniCssExtractPlugin({
+        filename: "static/css/[name].[contenthash:8].css",
+        chunkFilename: "static/css/[name].[contenthash:8].chunk.css",
+      })
+    );
+  }
+
+  return {
+    mode,
+    devtool: isProduction ? "source-map" : "eval-source-map",
+    output: {
+      path: isProduction ? buildDir : undefined,
+      filename: "static/js/[name].[contenthash:8].js",
+      chunkFilename: "static/js/[name].[contenthash:8].chunk.js",
+      publicPath: "/",
+      clean: true,
+    },
+    entry: "./src/index.tsx",
+    resolve: { extensions: [".ts", ".tsx", ".js", ".css"] },
+    module: {
+      rules: [
+        CSSRule,
+        {
+          test: /\.jpe?g|\.svg/,
+          loader: "file-loader",
+          options: {
+            name: "static/media/[name].[hash:8].[ext]",
           },
         },
-      }),
-    ],
-    splitChunks: {
-      chunks: "all",
-      cacheGroups: {
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          name: "vendor",
-          chunks: "initial",
+        { test: /\.tsx?$/, use: "ts-loader" },
+      ],
+    },
+    plugins,
+    optimization: {
+      minimize: isProduction,
+      minimizer: [
+        new CssMinimizerPlugin(),
+        new TerserPlugin({
+          terserOptions: {
+            parse: {
+              ecma: 8,
+            },
+            compress: {
+              ecma: 5,
+              warnings: false,
+              comparisons: false,
+              inline: 2,
+            },
+            output: {
+              ecma: 8,
+              comments: false,
+              ascii_only: true,
+            },
+          },
+        }),
+      ],
+      splitChunks: {
+        chunks: "all",
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: "vendor",
+            chunks: "initial",
+          },
         },
       },
     },
-  },
+  };
 };
