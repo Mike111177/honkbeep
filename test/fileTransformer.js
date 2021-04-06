37a243acfdd418eb8f16fcd5489b4407d4877ee2
Mike111177 <captain@mikeddavidson.com>
2021-04-06 19:33:50 -0400

    Fixed fileTransformer output

--- test/fileTransformer.js
+++ test/fileTransformer.js
@@ -3,5 +3,6 @@ const path = require("path");
 module.exports = {
   process(src, filename) {
     const assetFilename = JSON.stringify(path.basename(filename));
+    return `module.exports = ${assetFilename};`;
   },
 };
