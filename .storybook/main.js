4ad86e4bdd483d51f53f2986f56e96a6e9c13eb9
Mike111177 <captain@mikeddavidson.com>
2021-04-13 18:22:42 -0400

    Dependency Bump

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,9 +8,5 @@ module.exports = {
     };
   },
   stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-actions",
-  ],
+  addons: ["@storybook/addon-essentials"],
 };
