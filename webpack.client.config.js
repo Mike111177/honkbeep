c35341e548ccba1f603c6bb53c225dd3275ca109
Mike111177 <captain@mikeddavidson.com>
2021-05-02 16:43:58 -0400

    Cleaned up client.webpack.config

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -1,5 +1,10 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const SVGOConfig = require("./svgo.config");
 
 module.exports = (env) => {
@@ -7,8 +12,8 @@ module.exports = (env) => {
   const mode = env.production ? "production" : "development";
   const isProduction = mode === "production";
   const isDevelopment = !isProduction;
-
-  const namePatterns = isProduction
+  const useSourceMap = env.noMap ? false : true;
+  const Patterns = isProduction
     ? {
         JSModule: "static/[contenthash].js",
         JSChunk: "static/[contenthash].js",
@@ -26,133 +31,29 @@ module.exports = (env) => {
         CSSClass: "[local]-[hash:base64:5]",
       };
 
-  //Init rules
-  const CSSRule = {
-    test: /\.css$/,
-    //This will have MiniCssExtractPlugin.loader or "style-loader" injected into it
-    use: [
-      {
-        loader: "css-loader",
-        options: {
-          modules: {
-            localIdentName: namePatterns.CSSClass,
-          },
-        },
-      },
-    ],
-  };
-
-  const ImageRule = {
-    test: /\.(gif|png|jpe?g)$/i,
-    use: [
-      {
-        loader: "file-loader",
-        options: {
-          name: namePatterns.Image,
-        },
-      },
-    ],
-  };
-
-  const SVGRule = {
-    test: /\.svg$/,
-    use: [
-      {
-        loader: "@svgr/webpack",
-        options: {
-          svgo: SVGOConfig,
-        },
-      },
-      {
-        loader: "file-loader",
-        options: {
-          name: namePatterns.Image,
-        },
-      },
-    ],
-  };
-
-  const TypeScriptRule = {
-    test: /\.tsx?$/,
-    loader: "ts-loader",
-    options: { happyPackMode: true, transpileOnly: true },
-  };
-
   //Init Plugins
-  const plugins = [new HtmlWebpackPlugin({ template: "./src/index.html" })];
-
-  //Init minimizing settings
-  let minimizing = {};
-
-  //Things to add in development environment
-  if (isDevelopment) {
-    //Add typechecking
-    const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
-    plugins.push(new ForkTsCheckerWebpackPlugin());
-
-    //Make sure css is loaded without minification
-    CSSRule.use.unshift("style-loader");
-
-    //Disable minimizer
-    minimizing = { minimize: false };
-  }
-
-  //Things to add for production build
-  if (isProduction) {
-    const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-    const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-    const TerserPlugin = require("terser-webpack-plugin");
-
-    //Minify css
-    CSSRule.use.unshift(MiniCssExtractPlugin.loader);
-    plugins.unshift(
-      new MiniCssExtractPlugin({
-        filename: namePatterns.CSSModule,
-        chunkFilename: namePatterns.CSSChunk,
-      })
-    );
-
-    //Analyze bundles
-    const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
-      .BundleAnalyzerPlugin;
-    plugins.push(
-      new BundleAnalyzerPlugin({
-        analyzerMode: "disabled",
-        generateStatsFile: true,
-      })
-    );
-
-    //Enable minimizers
-    minimizing = {
-      minimize: true,
-      minimizer: [
-        new CssMinimizerPlugin(),
-        new TerserPlugin({
-          parallel: true,
-          terserOptions: {
-            compress: { ecma: 2018 },
-          },
-        }),
-      ],
-    };
-
-    //Compress Images
-    const ImageCompressLoader = {
-      loader: "image-webpack-loader",
-      options: {
-        svgo: SVGOConfig,
-      },
-    };
-    ImageRule.use.push(ImageCompressLoader);
-    SVGRule.use.push(ImageCompressLoader);
-  }
-
+  const plugins = [
+    isProduction
+      ? new MiniCssExtractPlugin({
+          filename: Patterns.CSSModule,
+          chunkFilename: Patterns.CSSChunk,
+        })
+      : null,
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    isDevelopment ? new ForkTsCheckerWebpackPlugin() : null,
+    isProduction
+      ? new BundleAnalyzerPlugin({
+          analyzerMode: "disabled",
+          generateStatsFile: true,
+        })
+      : null,
+  ].filter((i) => i);
   return {
     mode,
-    devtool: "source-map",
+    devtool: useSourceMap ? "source-map" : false,
     output: {
-      filename: namePatterns.JSModule,
-      chunkFilename: namePatterns.JSChunk,
+      filename: Patterns.JSModule,
+      chunkFilename: Patterns.JSChunk,
       path: path.resolve(__dirname, "build", "client"),
       publicPath: "/",
       clean: true,
@@ -160,15 +61,92 @@ module.exports = (env) => {
     entry: "./src/index.tsx",
     resolve: { extensions: [".ts", ".tsx", ".js", ".css"] },
     module: {
-      rules: [CSSRule, ImageRule, SVGRule, TypeScriptRule],
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                modules: {
+                  localIdentName: Patterns.CSSClass,
+                },
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(gif|png|jpe?g)$/i,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: Patterns.Image,
+              },
+            },
+            isProduction
+              ? {
+                  loader: "image-webpack-loader",
+                  options: {
+                    svgo: SVGOConfig,
+                  },
+                }
+              : null,
+          ].filter((i) => i),
+        },
+        {
+          test: /\.svg$/,
+          use: [
+            {
+              loader: "@svgr/webpack",
+              options: {
+                svgo: SVGOConfig,
+              },
+            },
+            {
+              loader: "file-loader",
+              options: {
+                name: Patterns.Image,
+              },
+            },
+            isProduction
+              ? {
+                  loader: "image-webpack-loader",
+                  options: {
+                    svgo: SVGOConfig,
+                  },
+                }
+              : null,
+          ].filter((i) => i),
+        },
+        {
+          test: /\.tsx?$/,
+          loader: "ts-loader",
+          options: { happyPackMode: true, transpileOnly: true },
+        },
+      ],
     },
     plugins,
     optimization: {
-      ...minimizing,
-      splitChunks: {
-        chunks: "all",
-      },
-      runtimeChunk: true,
+      ...(isDevelopment
+        ? { minimize: false }
+        : {
+            minimize: true,
+            minimizer: [
+              new CssMinimizerPlugin(),
+              new TerserPlugin({
+                parallel: true,
+                terserOptions: {
+                  compress: { ecma: 2018 },
+                },
+              }),
+            ],
+            splitChunks: {
+              chunks: "all",
+            },
+            runtimeChunk: true,
+          }),
     },
     performance: isProduction ? {} : false,
     stats: isProduction
@@ -185,9 +163,8 @@ module.exports = (env) => {
       historyApiFallback: true,
       hot: true,
       proxy: {
-        "/api": { target: "http://localhost:3001", logLevel: "silent" },
-        "/socket": {
-          target: "ws://localhost:3001",
+        "/api": {
+          target: "http://localhost:3001",
           logLevel: "silent",
           ws: true,
         },
