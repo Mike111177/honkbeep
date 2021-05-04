304adf38df35c67c587e7510a5a913d54327ee0e
Mike111177 <captain@mikeddavidson.com>
2021-05-04 19:24:07 -0400

    Simplified card home finding using new metadata

--- src/ui/HBBoard/AnimatedDeck/useSledCardState.ts
+++ src/ui/HBBoard/AnimatedDeck/useSledCardState.ts
@@ -1,24 +1,44 @@
+import { LocationType, Location } from "../../../client/types/Location";
 import * as ArrayUtil from "../../../util/ArrayUtil";
 import { useBoardStateSelector } from "../../BoardContext";
 import { ZonePath } from "../../Zone";
-import { getCardHome } from "./getCardHome";
+
+//May as well store this as a const
+const deckPath: ZonePath = ["deck"];
+
+function getCardHome(location: Location): ZonePath {
+  switch (location.place) {
+    case LocationType.Hand:
+      return ["hands", location.player, location.slot];
+    case LocationType.Stack:
+      return ["stacks", location.stack];
+    case LocationType.Discard:
+      return ["discard", location.order];
+    default:
+      return deckPath;
+  }
+}
 
 export function useSledCardState(
   index: number
 ): [boolean, boolean, boolean, ...ZonePath] {
   return useBoardStateSelector(
     ({ paused, viewTurn, hypothetical, playerOfTurn, myTurn }) => {
-      const home = getCardHome(index, viewTurn);
+      const { location } = viewTurn.cardMeta[index];
+      const home = getCardHome(location);
       const cardInCurrentPlayerHand =
-        home[0] === "hands" && home[1] === playerOfTurn;
+        location.place === LocationType.Hand &&
+        location.player === playerOfTurn;
       const stack =
-        home[0] === "stacks" ? viewTurn.stacks[home[1] as number] : undefined;
+        location.place === LocationType.Stack
+          ? viewTurn.stacks[location.stack]
+          : undefined;
       const cardOnTopOfStack =
         stack !== undefined && stack[stack.length - 1] === index;
       const visible =
         cardOnTopOfStack ||
         !(
-          home[0] === "deck" ||
+          location.place === LocationType.Deck ||
           (stack !== undefined && stack[stack.length - 2] !== index)
         );
       return [
